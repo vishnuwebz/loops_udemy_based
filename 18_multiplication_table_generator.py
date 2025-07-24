78a02f7cf88db51e69c46f5b76980182dc2aa678
Vishnu G <vishnugkannath@gmail.com>
2025-07-24 17:12:02 +0530

    Added multiplication table generator

---
+++ 18_multiplication_table_generator.py
@@ -0,0 +1,10 @@
+# 1 : Multiplication Table Generator
+
+def print_multiplication_table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+print_multiplication_table(5)
+
+# mini 2 - password retry system
+
+
